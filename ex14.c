764f664f0bc713536f5afc413f86dc09711c4f77
Riley Chapman <riley.chapman@getcruise.com>
2019-10-14 11:54:06 -0700

    exercise 14 with extra credit

--- ex14.c
+++ ex14.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[], int len);
 
 void print_arguments(int argc, char *argv[])
 {
     int i = 0;
 
     for(i = 0; i < argc; i++) {
-        print_letters(argv[i]);
+        print_letters(argv[i], strlen(argv[i]));
     }
 }
 
-void print_letters(char arg[])
+void print_letters(char arg[], int len)
 {
     int i = 0;
 
-    for(i = 0; arg[i] != '\0'; i++) {
+    for(i = 0; i != len; i++) {
         char ch = arg[i];
 
         if(can_print_it(ch)) {
@@ -31,8 +32,7 @@ void print_letters(char arg[])
 
 int can_print_it(char ch)
 {
-    //return isalpha(ch) || isblank(ch);
-    return 1;
+    return isalpha(ch) || isblank(ch) || isdigit(ch) || ispunct(ch);
 }
 
 int main(int argc, char *argv[])
